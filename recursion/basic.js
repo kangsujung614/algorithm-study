e5cf1b4b134720b120a167ccaf8da31578190769
kangsujung614 <sjkang614@gmail.com>
2024-09-22 23:26:51 +0900

    DFS(재귀) 알고리즘 문제 정답 분석

--- recursion/basic.js
+++ recursion/basic.js
@@ -28,3 +28,31 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+// 프로그래머스 코딩테스트 연습 > 타겟 넘버 > DFS, 재귀
+// 다른 사람의 풀이한 정답 코드 분석
+function solution(numbers, target) {
+    let answer = 0;
+
+    const dfs = (idx, result) =>{
+        console.log(idx, result);
+        if(idx === numbers.length && result === target){
+            answer += 1;
+            console.log("통과");
+            return; // 함수의 실행을 중단하고 반환함으로써 재귀 호출 중단
+        }
+        else if(idx === numbers.length && result !== target){
+            console.log("탈락");
+            return; // 함수의 실행을 중단하고 반환함으로써 재귀 호출 중단
+        }
+        // 완전 이진 트리로 접근하여 이해(binaryTree.png)
+        dfs(idx + 1, result - numbers[idx]); // 재귀
+        dfs(idx + 1, result + numbers[idx]); // 재귀
+    }
+
+    dfs(0, 0);
+
+    return answer;
+}
+
+console.log(solution([1, 1, 1, 1, 1], 3));
